f2b43ab6397e2917dfb643dc3ed66619e052ea22
Son Roy Almerol <sonroya.almerol@gmail.com>
2022-06-04 14:15:58 -0400

    finish assignment 1

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -7,3 +7,22 @@
   date: May 20, 2022
 
   =============== */
+
+  const processContactForm = (e) => {
+    if ('preventDefault' in e) e.preventDefault()
+    
+    const formData = new FormData(document.getElementById('contact-form'))
+    var object = {}
+    formData.forEach((value, key) => object[key] = value)
+
+    window.alert(JSON.stringify(object))
+
+    return false
+  }
+
+  const contactForm = document.getElementById('contact-form')
+  if ('attachEvent' in contactForm) {
+    contactForm.attachEvent('submit', processContactForm)
+  } else {
+    contactForm.addEventListener('submit', processContactForm)
+  }
